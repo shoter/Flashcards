f55ad4f8b73716ce97805cd3f4d5b6eaa8a791bc
shoter <lubiebardzozelki@gmail.com>
2017-10-16 00:07:36 +0200

    Initial

---
+++ Flashcards/Flashcards/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Flashcards
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
